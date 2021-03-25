f28d1b693d83d835aa73f2c942c8f91f73c37283
nroldanr <nroldanr@eafit.edu.co>
2021-03-24 19:12:10 -0500

    models

--- chat_room/room/consumers.py
+++ chat_room/room/consumers.py
@@ -1,11 +1,15 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from .models import Room
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+
+        if not Room.objects.filter(roomName = self.room_name):
+            Room(roomName = self.room_name, roomTopic = 'Charlas').save()
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
--- chat_room/room/models.py
+++ chat_room/room/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    roomName = models.CharField(max_length=20, unique=True)
+    roomTopic = models.CharField(max_length=50)
+
+class Message(models.Model):
+    messContent = models.CharField(max_length=500)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
